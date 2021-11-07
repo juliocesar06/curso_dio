ace7642e52b65f49eab9b980ce317d058e845c9f
juliocesar06 <julio06cesar11@gmail.com>
2021-11-07 09:22:27 -0300

    atualização

--- curso_jquery_ajax/main.js
+++ curso_jquery_ajax/main.js
@@ -1,8 +1,11 @@
 function consultaCep() {
   let cepi = document.getElementById("cep");
+  let url = "http://viacep.com.br/ws/" + cepi.value + "/json";
+  alert(cepi);
+  alert(url);
 
   $.ajax({
-    url: `https://viacep.com.br/ws/${cepi.value}/json/`,
+    url: url,
     type: "GET",
     success: function (response) {
       console.log(response);
